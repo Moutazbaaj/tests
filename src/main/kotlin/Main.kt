97a12b4141146cda0443e5dc92ab0991cc17329d
Moutazbaaj <148048629+Moutazbaaj@users.noreply.github.com>
2023-11-16 22:31:59 +0100

    ..

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,20 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+//Benutze für folgende Aufgabe das Try&Catch Prinzip.
+//Erstelle eine Variable  Zahlstring vom Typ String und initialisiere sie  mit  einem Zahlenwert mittels readln().
+// Wichtig ist, dass der Zahlenwert als Text geschrieben wird als eins, zwei, zehn usw.
+//Erstelle einen neue Variabel ZahlInt vom Typ Int und gebe ihr den Wert den du per readln() eingegeben hast.
+//Fange in einem Catch Block die Exception ab und schreibe in eine Println warum es zu dieser Exception gekommen ist.
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+fun main() {
+
+    println("Bitte gib einen Zahlenwert als Text ein:")
+    val zahlString: String = readln()
+    try {
+        val zahlInt: Int = zahlString.toInt()
+        println("Die eingegebene Zahl als Integer: $zahlInt")
+
+    } catch (e: Exception) {
+        println("Fehler: Die Eingabe '$zahlString' konnte nicht in eine Zahl umgewandelt werden.")
+    }
+
 }
